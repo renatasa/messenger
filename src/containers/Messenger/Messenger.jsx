2b9214a744838855ae5ce63cc1fe2b20526b5e31
Begemotas <cheminiselementas@gmail.com>
2021-06-14 12:57:43 +0300

    Messenger.jsx comments cleaned, componentWillUnmount removed

--- src/containers/Messenger/Messenger.jsx
+++ src/containers/Messenger/Messenger.jsx
@@ -3,7 +3,6 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import InputField from "../../components/UI/ChatInputField/ChatInputField";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Navbar from "../../components/Navbar/Navbar";
-import axios from "axios";
 import { connect } from "react-redux";
 import classes from "./Messenger.module.css";
 import {
@@ -12,24 +11,13 @@ import {
   createSpinner,
   addNewContact,
   sendMessage,
-  redirectToLogin
+  redirectToLogin,
 } from "./service";
 import { chatsDataGetRequest, sendPutRequest } from "../api/messengerApi";
 import { constants } from "./constants";
 
 export class Messenger extends Component {
-  // data - chatting data that is received from backend
-  // by default, when first loads,
-  // messenger displays first chat (received from backend data array) in Messaging section
-  // therefore selectedChat by default is 0
-  // newMessage - this is text that user types in input field
-  // newContact - text that user types in Sidebar input field when adding new contact
-  // errorLoadingChats - HTTP GET request error
-  // errorSendingMessage - HTTP POST request error
-  // errorAddingContact - HTTP POST request error
-  // errorSendingMessage and errorAddingContact differ in css
-  // showSidebar - used in mobile responsie devices, when switching between sidebar view and chat view
-
+  // different css is applied for different type of error
   state = {
     data: null,
     selectedChat: 0,
@@ -41,55 +29,10 @@ export class Messenger extends Component {
     showSidebar: true,
   };
 
-  // sendPutRequest = (newData, clearInput, updateError) => {
-  //   const newDataObj = {
-  //     data: newData,
-  //   };
-
-  //   const newDataJson = JSON.stringify(newDataObj);
-
-  //   const useHeaders = {
-  //     headers: {
-  //       "secret-key": process.env.REACT_APP_API_KEY,
-  //       "Content-Type": "application/json",
-  //       versioning: "false",
-  //     },
-  //   };
-
-  //   axios
-  //     .put(process.env.REACT_APP_GET_CHATS, newDataJson, useHeaders)
-  //     .then((response) => {
-  //       this.checkRequestStatusUpdateState(
-  //         response,
-  //         newData,
-  //         updateError,
-  //         clearInput
-  //       );
-  //     })
-
-  //     .catch((error) => {
-  //       console.log(error);
-  //       this.checkRequestStatusUpdateState(
-  //         error.response,
-  //         null,
-  //         updateError,
-  //         clearInput
-  //       );
-  //     });
-  // };
-
-  // this.sendContinuousRequestsUpdateChats function is used for updating this.state.data
-  // and chat UI if there are any new messages sent by other users. It is set to execute every 2s.
-  // this.sendContinuousRequestsUpdateChats is eliminated, when Messenger component unmounts.
-  // currently this.sendContinuousRequestsUpdateChats  and componentWillUnmount are commented
-  // to prevent exceeding amount of free requests of JSONbin.io
   componentDidMount() {
     if (this.props.email !== null && this.props.password !== null) {
       chatsDataGetRequest(this.checkRequestStatusUpdateState);
     }
-    //  this.sendContinuousRequestsUpdateChats = setInterval(() => {
-    //   this.chatsDataGetRequest();
-    // }, 2000);
   }
 
   componentDidUpdate() {
@@ -102,18 +45,9 @@ export class Messenger extends Component {
     }
   }
 
-  componentWillUnmount() {
-    //  clearInterval(this.sendContinuousRequestsUpdateChats);
-  }
-
   checkRequestStatusUpdateState = (req, newData, error, clearInput) => {
-    // if request status !=200 , this.state.data does not update, one of error properties in state updates
-    // when fetching data from backend, emptying user's input fields in UI is undesirable, therefore it is not updated with setState
-    // when sending new text message, we want to clear input field after successful request (set this.state.newMessage="")
-    // after new contact is added, we want to  clear input field after successful request (set this.state.newContact="")
     if (req.status !== 200) {
       this.setState({ [error]: req["data"]["message"] });
-      // this.setState({ [error]: JSON.parse(req.response).message });
     } else if (clearInput === constants.doNotClearInput) {
       this.setState({
         data: newData,
@@ -128,21 +62,11 @@ export class Messenger extends Component {
     }
   };
 
-  // when user clicks on a contact name in the Sidebar,
-  // selectChat function updates selectedChat property in state
-  // therefore, messagingSection array
-  // updates and displays chat with selected user
   selectChat = (index) => {
     this.setState({ selectedChat: index });
-
-    // following function hides sidebar on mobile devices
-    // when this.state.showSidebar is false,
-    // Sidebar is assigned CSS class with display: none property
     this.hideSidebar();
   };
 
-  // updates newMessage property in state object
-  // when user starts typing new message or adding new contact into textarea or input field
   inputChangedHandler = (event, inputName) => {
     event.preventDefault();
     this.setState({ [inputName]: event.target.value });
@@ -152,8 +76,6 @@ export class Messenger extends Component {
     this.setState({ [errorType]: null });
   };
 
-  // showSidebarFunction and hideSidebar are used in mobile devices for switching
-  // between sidebar view and chat view
   showSidebarFunction = () => {
     this.setState({ showSidebar: true });
   };
@@ -190,7 +112,7 @@ export class Messenger extends Component {
                 addNewContact(
                   this.state.newContact,
                   this.state.data,
-                  sendPutRequest, 
+                  sendPutRequest,
                   this.checkRequestStatusUpdateState
                 )
               }
@@ -239,7 +161,7 @@ export class Messenger extends Component {
                   this.state.errorSendingMessage,
                   this.state.data,
                   this.state.selectedChat,
-                  sendPutRequest, 
+                  sendPutRequest,
                   this.checkRequestStatusUpdateState
                 )
               }
